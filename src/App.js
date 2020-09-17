cff84796261f4eab11d80363d4f92f5c7d7a1dce
Ngo Le Nhat Minh <minh.nln@kiwi-universe.com>
2020-09-18 00:52:42 +0700

    Refine tab

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from "react";
 import "./App.css";
-import HomeIcon from "@material-ui/icons/Home";
-import AddCommentIcon from "@material-ui/icons/AddComment";
-import BorderClearIcon from "@material-ui/icons/BorderClear";
-import DateRangeIcon from "@material-ui/icons/DateRange";
-import Button from "@material-ui/core/Button";
 
 import Header from "./components/Header";
 import NavBar from "./components/NavBar";
 import EnhancedTable from "./components/EnhancedTable";
+
+import CompanyIcon from "@material-ui/icons/Business";
+import AddIcon from "@material-ui/icons/Add";
 
 function App() {
   return (
@@ -23,45 +21,21 @@ function App() {
 const tabs = [
   {
     id: 1,
-    label: "Contacts",
-    component: <h1>Contacts</h1>,
-    icon: <AddCommentIcon />,
+    label: "All Companies",
+    component: <h1>All Companies</h1>,
+    icon: <CompanyIcon />,
   },
   {
     id: 2,
-    label: "Conversations",
-    component: <h1>Conversations</h1>,
-    icon: <AddCommentIcon />,
+    label: "My Companies",
+    component: <h1>My Companies</h1>,
+    icon: <CompanyIcon />,
   },
   {
     id: 3,
-    label: "Marketing",
-    component: <h1>Marketing</h1>,
-    icon: <BorderClearIcon />,
-  },
-  {
-    id: 4,
-    label: "Sales",
-    component: <h1>Sales</h1>,
-    icon: <DateRangeIcon />,
-  },
-  {
-    id: 5,
-    label: "Services",
-    component: <h1>Services</h1>,
-    icon: <DateRangeIcon />,
-  },
-  {
-    id: 6,
-    label: "Automation",
-    component: <h1>Automation</h1>,
-    icon: <DateRangeIcon />,
-  },
-  {
-    id: 7,
-    label: "Reports",
-    component: <h1>Reports</h1>,
-    icon: <DateRangeIcon />,
+    label: "Add View",
+    component: <h1>Add View</h1>,
+    icon: <AddIcon />,
   },
 ];
 
